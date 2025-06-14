335e7035bee386c9cfae8ded7baf8cefaad2ae12
nguyenhongquan <emtenquan012@gmail.com>
2025-06-14 16:05:14 +0700

    config Program to deploy swaggerUI

--- .net/PhyGen_SWD392/Program.cs
+++ .net/PhyGen_SWD392/Program.cs
@@ -22,13 +22,19 @@ builder.Services.AddDbContext<PhyGenDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;  
+});
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
